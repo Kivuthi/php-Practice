3772312d676a19eca76e0522e1702a8a9e96a980
West Side <officialdee77@gmail.com>
2025-08-29 11:38:41 +0300

    update

---
+++ index.php
@@ -0,0 +1,26 @@
+<!-- day 1 exercise -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php session</title>
+</head>
+<body>
+    <h1>This is me trying php</h1>
+
+<?php
+echo "<p> my name is Dennis Musyoka </p>";
+
+echo " <p>Today's date is " . date("Y/m/d") . "</p>";
+
+// another date format
+
+echo " <p> another format is " . date("d-m-Y") . "</p>";
+
+echo date("l, F j, Y");
+?>
+
+</body>
+</html>
+
